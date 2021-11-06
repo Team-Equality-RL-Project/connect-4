29938470e0039ef60dd73d39e5ba8be92852f276
Shannon Phu <shannonphu@gmail.com>
2021-11-05 17:08:06 -0700

    moving graphics code to renderer class

--- src/connect4.py
+++ src/connect4.py
@@ -1,120 +1,71 @@
 import random
-import pygame
-import sys
 import math
 
 from board import Board
+from renderer import Renderer
 from minimax import Minimax
 
-BLUE = (0,0,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-YELLOW = (255,255,0)
+# Setup game
+
+game_over = False
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
-
-PLAYER = 0
-AI = 1
+board = Board(ROW_COUNT, COLUMN_COUNT)
+board.print()
 
 EMPTY = 0
 PLAYER_PIECE = 1
 AI_PIECE = 2
-
-def draw_board(board):
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT):
-			pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
-			pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
-	
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT):		
-			if board[r][c] == PLAYER_PIECE:
-				pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-			elif board[r][c] == AI_PIECE: 
-				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-	pygame.display.update()
-
-board = Board(ROW_COUNT, COLUMN_COUNT)
-board.print()
-game_over = False
-
-algo = Minimax()
-
-pygame.init()
-
-SQUARESIZE = 100
-
-width = COLUMN_COUNT * SQUARESIZE
-height = (ROW_COUNT+1) * SQUARESIZE
-
-size = (width, height)
-
-RADIUS = int(SQUARESIZE/2 - 5)
-
-screen = pygame.display.set_mode(size)
-draw_board(board.get_state())
-pygame.display.update()
-
-myfont = pygame.font.SysFont("monospace", 75)
+PLAYER = 0
+AI = 1
+renderer = Renderer(ROW_COUNT, COLUMN_COUNT, PLAYER_PIECE, AI_PIECE)
+renderer.draw(board.get_state())
 
 turn = random.randint(PLAYER, AI)
 
+# Define RL algo
+
+algo = Minimax()
+
+# Gameplay loop
+
 while not game_over:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
+	if turn == PLAYER:
+		# Get frame and events from pygame graphics renderer
+		clicked_col = renderer.handle_events()
+		if clicked_col:
+			if board.is_valid_location(clicked_col):
+				row = board.get_next_open_row(clicked_col)
+				board.drop_piece(row, clicked_col, PLAYER_PIECE)
 
-		if event.type == pygame.MOUSEMOTION:
-			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-			posx = event.pos[0]
-			if turn == PLAYER:
-				pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+				if board.is_winning_move(PLAYER_PIECE):
+					print('Player won')
+					game_over = True
 
-		pygame.display.update()
+				turn += 1
+				turn = turn % 2
 
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-			# Ask for Player 1 Input
-			if turn == PLAYER:
-				posx = event.pos[0]
-				col = int(math.floor(posx/SQUARESIZE))
+				board.print()
+				renderer.draw(board.get_state())
 
-				if board.is_valid_location(col):
-					row = board.get_next_open_row(col)
-					board.drop_piece(row, col, PLAYER_PIECE)
-
-					if board.is_winning_move(PLAYER_PIECE):
-						label = myfont.render("Player 1 wins!!", 1, RED)
-						screen.blit(label, (40,10))
-						game_over = True
-
-					turn += 1
-					turn = turn % 2
-
-					board.print()
-					draw_board(board.get_state())
-
-
-	# Ask for Player 2 Input
-	if turn == AI and not game_over:				
+	if turn == AI and not game_over:
+		# Use RL to get the best next move
 		col, minimax_score = algo.get_best_move(board, 5, -math.inf, math.inf, True)
 
 		if board.is_valid_location(col):
-			#pygame.time.wait(500)
 			row = board.get_next_open_row(col)
 			board.drop_piece(row, col, AI_PIECE)
 
 			if board.is_winning_move(AI_PIECE):
-				label = myfont.render("Player 2 wins!!", 1, YELLOW)
-				screen.blit(label, (40,10))
+				print('AI won')
 				game_over = True
 
 			board.print()
-			draw_board(board.get_state())
+			renderer.draw(board.get_state())
 
 			turn += 1
 			turn = turn % 2
 
-	if game_over:
-		pygame.time.wait(3000)
+	# if game_over:
+		# pygame.time.wait(3000)
---
+++ src/renderer.py
@@ -0,0 +1,90 @@
+import pygame
+import sys
+import math
+
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+SQUARESIZE = 100
+
+class Renderer:
+    def __init__(self, n_rows, n_cols, piece_a, piece_b):
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.width = n_cols * SQUARESIZE
+        self.height = (n_rows+1) * SQUARESIZE
+        self.size = (self.width, self.height)
+        self.radius = int(SQUARESIZE / 2 - 5)
+        self.piece_a = piece_a # player piece
+        self.piece_b = piece_b # AI piece
+
+        # myfont = pygame.font.SysFont("monospace", 75)
+
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.size)
+        self.draw_board()
+        pygame.display.update()
+    
+    def handle_exit(self):
+        sys.exit()
+    
+    def handle_motion(self, event, turn):
+        pygame.draw.rect(self.screen, BLACK, (0,0, self.width, SQUARESIZE))
+        posx = event.pos[0]
+        # if turn == self.piece_a:
+        pygame.draw.circle(self.screen, RED, (posx, int(SQUARESIZE/2)), self.radius)
+    
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(self.screen, BLACK, (0,0, self.width, SQUARESIZE))
+                posx = event.pos[0]
+                pygame.draw.circle(self.screen, RED, (posx, int(SQUARESIZE/2)), self.radius)
+
+            pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(self.screen, BLACK, (0,0, self.width, SQUARESIZE))
+
+                # Ask for Player 1 Input
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARESIZE))
+                return col
+                
+        return None
+    
+    def handle_click(self, board):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print('clicked!')
+                pygame.draw.rect(self.screen, BLACK, (0,0, self.width, SQUARESIZE))
+                # Ask for Player 1 Input
+                posx = event.pos[0]
+                col = int(math.floor(posx / SQUARESIZE))
+
+                if board.is_valid_location(col):
+                    return col
+            pygame.display.update()
+        return None
+
+    def draw_board(self):
+        for c in range(self.n_cols):
+            for r in range(self.n_rows):
+                pygame.draw.rect(self.screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+                pygame.draw.circle(self.screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), self.radius)
+        pygame.display.update()
+
+    def draw(self, board_state):
+        self.draw_board()
+
+        for c in range(self.n_cols):
+            for r in range(self.n_rows):
+                if board_state[r][c] == self.piece_a:
+                    pygame.draw.circle(self.screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), self.height-int(r*SQUARESIZE+SQUARESIZE/2)), self.radius)
+                elif board_state[r][c] == self.piece_b: 
+                    pygame.draw.circle(self.screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), self.height-int(r*SQUARESIZE+SQUARESIZE/2)), self.radius)
+        pygame.display.update()
